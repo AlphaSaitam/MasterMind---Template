eeb0379aa812ff1486edb51b3f903e231c7bdfe4
AlphaSaitam <97667455+AlphaSaitam@users.noreply.github.com>
2022-02-25 16:54:52 +0100

    V2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int[] array = new int[3];
+    
     void Start()
     {
         
