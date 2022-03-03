edee4da6d791832d5b924e74867c54f0df145a07
AlphaSaitam <97667455+AlphaSaitam@users.noreply.github.com>
2022-03-03 16:49:07 +0100

    color

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int[] array = new int[3];
+    public int[] array = new int[4];
+    public Color[] Colors = {Color.red, Color.blue, Color.green, Color.yellow};
+   
     
+
     void Start()
     {
+        Colors[0] = Color.red;
+        Colors[1] = Color.blue;
+        Colors[2] = Color.green;
+        Colors[3] = Color.yellow;
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+       
     }
 }
